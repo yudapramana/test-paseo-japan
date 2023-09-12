5f09eadddddbcc9e404691126c50327240498049
pramana yuda <pramanayuda772@gmail.com>
2023-09-12 20:39:03 +0700

    push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
 
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
